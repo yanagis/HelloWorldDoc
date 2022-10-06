60a719fb88c4809d08005a356223a9ab75d0e08c
Hiroki Yanagisawa <yanagis@jp.ibm.com>
2022-10-06 15:12:13 +0900

    nojekyll

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,7 @@ author = 'Hiroki Yanagisawa'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.mathjax', 'sphinx.ext.todo', 'sphinx.ext.githubpages']
 
 templates_path = ['_templates']
 exclude_patterns = []
